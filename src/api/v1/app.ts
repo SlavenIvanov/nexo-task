65748d660ff3bd62b4e0a20dc0bd1c8aaec3018a
Slaven Ivanov <slaven.ivanov.95@gmail.com>
2024-09-17 20:36:19 +0300

    added swagger docs

--- src/api/v1/app.ts
+++ src/api/v1/app.ts
@@ -1,14 +1,22 @@
 import Fastify from 'fastify'
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod'
 import { API_PREFIX, API_VERSION } from '../../util/constants'
 import { handleHttpError } from './error.handlers'
 import { filtersRoutes } from './filters/filters.routes'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 
 export async function buildApp() {
   const app = Fastify().withTypeProvider<ZodTypeProvider>()
 
   app.setValidatorCompiler(validatorCompiler)
   app.setSerializerCompiler(serializerCompiler)
+
+  app.register(fastifySwagger, { transform: jsonSchemaTransform })
+
+  app.register(fastifySwaggerUI, {
+    routePrefix: '/documentation'
+  })
 
   app.setErrorHandler(handleHttpError)
 
